1b4537a01bc321861e242f3d38f6c51f4b0784d8
Ricardo <ricardo.depena@gmail.com>
2016-07-23 13:44:16 -0400

    added indicator led, copy of rc.local to enable auto launch

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 RPI_LGN=pi
 RPI_IP=192.168.1.215
 FOLDER=/home/pi/Documents/Projects/pi-booth
+
 rsync -rav * $RPI_LGN@$RPI_IP:$FOLDER
--- pbooth.py
+++ pbooth.py
@@ -5,24 +5,35 @@ from subprocess import call
 from picamera import PiCamera
 
 BUTTON_PIN = 18
-home = os.environ['HOME']
+LED_PIN = 17
+HOME = '/home/pi'
 IMG_FOLDER = "photo_booth"
-UPLOADER_CMD = "/home/pi/Documents/Projects/Dropbox-Uploader/dropbox_uploader.sh upload"
+UPLOADER_CMD = os.path.join(HOME, "Documents/Projects/Dropbox-Uploader/dropbox_uploader.sh upload")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED_PIN, GPIO.OUT)
 camera = PiCamera()
 camera.resolution = (1024, 768)
+camera.image_effect = "none"
 
 def make_timestamp():
 	return time.strftime("%a%d%b%Y%H%M%S", time.gmtime())
 
 def make_image_location(name):
-	return os.path.join(home, IMG_FOLDER, name)
+	return os.path.join(HOME, IMG_FOLDER, name)
 
 def updload_picture(img_name, img_path):
 	upload = UPLOADER_CMD + " " + img_path + " " + img_name
 	call([upload], shell=True)
+
+def count_down(duration, intervals):
+	while(intervals > 0):
+		GPIO.output(LED_PIN, GPIO.HIGH)
+		time.sleep(duration)
+		GPIO.output(LED_PIN, GPIO.LOW)
+		time.sleep(duration)
+		intervals -=1
 
 def take_picture():
 	img_name = make_timestamp() + '.jpg'
@@ -33,6 +44,8 @@ def take_picture():
 while True:
 	input_state = GPIO.input(BUTTON_PIN)
 	if input_state == False:
-		print ("Taking picture!!")
+		count_down(0.5, 3)
 		take_picture()
-		print ("Done!!")
+		#indication that all is done
+		count_down(0.2, 3)
+	time.sleep(0.3)
